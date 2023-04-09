5893b7b0164f34a6a67b2ab0972eef9ace4eaf56
LucSilveira <lucas1-portal@hotmail.com>
2023-04-09 20:51:04 -0300

    Constantes

---
+++ estudo_java/Constantes/src/App.java
@@ -0,0 +1,11 @@
+public class App
+{
+    public static void main(String[] args)
+    {
+        // Para aplicar constantes que são variáveis imutáveis
+        // utilize o termo final ( similar a const )
+        final int variavel1 = 2;
+
+        System.out.println(variavel1);
+    }
+}
