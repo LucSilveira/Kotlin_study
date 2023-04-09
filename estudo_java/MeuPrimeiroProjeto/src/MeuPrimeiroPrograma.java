2b617110f8b31793387cea6ef0c6d5d5b9d7695e
LucSilveira <lucas1-portal@hotmail.com>
2023-04-09 18:05:13 -0300

    Finalizando o projeto de teste do IntelliJ

--- estudo_java/MeuPrimeiroProjeto/src/MeuPrimeiroPrograma.java
+++ estudo_java/MeuPrimeiroProjeto/src/MeuPrimeiroPrograma.java
@@ -3,11 +3,18 @@ public class MeuPrimeiroPrograma
     public static void main(String[] args)
     {
         System.out.println( MinhaNovaFuncao("lucao") );
+        MaisUmaFuncao(22);
     }
 
     // Criando minha própria função
     static String MinhaNovaFuncao(String nome)
     {
         return "Hello World, bem vindo : " + nome;
+    }
+
+    // Criando uma nova função pra idade
+    static void MaisUmaFuncao(int idade)
+    {
+        System.out.println("Você tem " + idade + " anos");
     }
 }
