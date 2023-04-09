fb90331ec5e98faa1fa6cf088bbf3d808b4cb0af
LucSilveira <lucas1-portal@hotmail.com>
2023-04-09 13:01:07 -0300

    Estudo sobre Java

---
+++ estudo_java/MeuPrimeiroProjeto/src/MeuPrimeiroPrograma.java
@@ -0,0 +1,13 @@
+public class MeuPrimeiroPrograma
+{
+    public static void main(String[] args)
+    {
+
+    }
+
+    // Criando minha própria função
+    void MinhaNovaFuncao()
+    {
+        System.out.println("Hello World");
+    }
+}
