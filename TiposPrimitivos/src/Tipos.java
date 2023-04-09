24a4babbadfc3fbeafe70ef4f4871a6652847e67
LucSilveira <lucas1-portal@hotmail.com>
2023-04-09 20:42:06 -0300

    Exemplos de tipos primitivos

---
+++ TiposPrimitivos/src/Tipos.java
@@ -0,0 +1,14 @@
+public class Tipos
+{
+    public static void main(String[] args)
+    {
+        // Tipos primitivos são espaços de memorias mais simples
+        // usando eles, a aplicação fica um pouco mais leve
+        // do que apenas utilizar variáveis normal para receber as informações
+        int x = 2;
+        Integer y = 4;
+
+        System.out.println(x);
+        System.out.println(y);
+    }
+}
